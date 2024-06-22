55120456d94219ecc04b59fc706070f73d0342e3
AngelTravieso <angeltraviesoc@gmail.com>
2024-06-22 14:16:38 -0400

    Agregar archivos de proyecto.

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hola Mundo");
+            Console.WriteLine("Hola Angel");
+            Console.Read();
+
+        }
+    }
+}
